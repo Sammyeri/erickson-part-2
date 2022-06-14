46152e8ca05a9514e1412f9847714f12fa81f482
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-06-14 19:06:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let name =prompt("Welcome to GC mini golf! What is your name?");
+let numberholes =prompt(`Hi, ${name}! Would you like to play 3 or 6 holes today?`);
+let putts=0
+
+if (numberholes == 3){
+    let par =9;
+    for (let i=1; i<=3; i++){
+        putts += parseInt(prompt(`How many putts for hole ${i}?`));
+    }
+    if (putts < par) {
+        under = par - putts;
+        console.log(`Great job, ${name}! your total par was -${under}`);
+    } else if (putts == par) {
+        console.log(`Good game, ${name}. Your total par was: 0.`);
+    } else {
+        over = putts - par;
+        console.log(`Nice try, ${name}... Your total par was: +${over}.`);
+    }
+    console.log(`${numberholes}, ${putts}`);
+} else if (numberholes == 6){
+    let par =18;
+    for (let i=1; i<=6; i++){
+        putts += parseInt(prompt(`How many putts for hole ${i}?`))
+    }
+    if (putts < par) {
+        under = par - putts;
+        console.log(`Great job, ${name}! your total par was -${under}`);
+    } else if (putts == par) {
+        console.log(`Good game, ${name}. Your total par was: 0.`);
+    } else {
+        over = putts - par;
+        console.log(`Nice try, ${name}... Your total par was: +${over}.`);
+    }
+    console.log(`${numberholes}, ${putts}`);
+} else {
+    console.log(`Im sorry, we do not have ${numberholes} holes, please try again.`);
+}
+
